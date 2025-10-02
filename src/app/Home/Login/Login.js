f61dbee446f7f5e1e7438ffdd83d457ef0372e3c
Ngoc Thach <ngocthachtruongnguyen@gmail.com>
2025-10-02 18:38:41 +0700

    fix Login for deploy

--- src/app/Home/Login/Login.js
+++ src/app/Home/Login/Login.js
@@ -1,6 +1,6 @@
 import api from "../../../Api/api";
 
-export const login = async (email, password, rememberMe = true) => {
+export const Login = async (email, password, rememberMe = true) => {
   const response = await api.post("Auth/login-user", {
     email,
     password,
