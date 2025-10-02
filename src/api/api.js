a3a87b8c72c09e2f7ad0cf2fde0292c006cbae0c
Trương Bảo Ngọc <ngoctbse183713@fpt.edu.vn>
2025-10-03 00:28:05 +0700

    url test fix

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: import.meta.env.VITE_API_URL,
-  baseURL: "http://api.metrohcmc.xyz/api",
+  baseURL: "https://api.metrohcmc.xyz/api",
 });
 
 // Request interceptor để tự động thêm JWT token
