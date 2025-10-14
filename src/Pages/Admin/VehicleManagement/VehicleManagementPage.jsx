e10ce18c824218968320cb3745e9041a3adc4a60
YoungLee2004 <144761712+YoungLee2004@users.noreply.github.com>
2025-10-14 16:04:00 +0700

    Clean Code Dealet some useless file

--- src/Pages/Admin/VehicleManagement/VehicleManagementPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import VehicleManagement from './VehicleManagement';
-
-// Wrapper component để sử dụng VehicleManagementNew
-function VehicleManagementPage() {
-  return <VehicleManagement />;
-}
-
-export default VehicleManagementPage;
